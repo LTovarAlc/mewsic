4c876b2f44dfe34bab19e9717bb53755ab4b041a
Luis Tovar <ltovaralc@gmail.com>
2024-11-06 17:46:52 -0600

    estructura de similar song agregada

--- src/components/searchFeed/searchFeed.jsx
+++ src/components/searchFeed/searchFeed.jsx
@@ -1,20 +1,54 @@
 import React from "react";
-import "./searchFeed.css"
+import "./searchFeed.css";
 import QueryCardSong from "../shared/queryCard/queryCard";
 
 const SearchFeed = () => {
-    return(
-        <section className="searchFeed">
-            <div className="searchFeed-Head">
-                <div className="BestResult">
-                    <QueryCardSong/>
-                </div>
-                <div className="SimilarResults">
-                    Similares
-                </div>
+  return (
+    <section className="searchFeed">
+      <div className="searchFeed-Head">
+        <div className="BestResult">
+          <QueryCardSong />
+        </div>
+        <div className="SimilarResults">
+          <div className="similarsCards">
+            <div className="similarSongImg"></div>
+            <div className="similarSong-details">
+              <p className="similarSongTitle">Song</p>
+              <span className="similarSongArtist">Artist</span>
             </div>
-        </section>
-    )
-}
+          </div>
+          <div className="similarsCards">
+            <div className="similarSongImg"></div>
+            <div className="similarSong-details">
+              <p className="similarSongTitle">Song</p>
+              <span className="similarSongArtist">Artist</span>
+            </div>
+          </div>
+          <div className="similarsCards">
+            <div className="similarSongImg"></div>
+            <div className="similarSong-details">
+              <p className="similarSongTitle">Song</p>
+              <span className="similarSongArtist">Artist</span>
+            </div>
+          </div>
+          <div className="similarsCards">
+            <div className="similarSongImg"></div>
+            <div className="similarSong-details">
+              <p className="similarSongTitle">Song</p>
+              <span className="similarSongArtist">Artist</span>
+            </div>
+          </div>
+          <div className="similarsCards">
+            <div className="similarSongImg"></div>
+            <div className="similarSong-details">
+              <p className="similarSongTitle">Song</p>
+              <span className="similarSongArtist">Artist</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default SearchFeed
+export default SearchFeed;
