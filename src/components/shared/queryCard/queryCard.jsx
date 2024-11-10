6bfdceaf0fec466f83220c5020ca826c6a370b3d
Luis Tovar <ltovaralc@gmail.com>
2024-11-10 14:40:42 -0600

    button play en query card solucionado

--- src/components/shared/queryCard/queryCard.jsx
+++ src/components/shared/queryCard/queryCard.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "./queryCard.css";
-import QueryCardPlayButton from "../queryCardPlayButton/queryCardPlayButtton";
+import PlayIMG from "../../../assets/imgs/tocar.png"
 
 const QueryCardSong = ({ songName, artistName, imageUrl, onClick }) => {
-
   return (
     <div className="QueryCardSong" onClick={onClick}>
-      {imageUrl && <img src={imageUrl} alt="SongIMG" className="QueryCard-SongIMG" />}
-      {songName && <p className="QueryCard-SongName">{songName}</p>}
-      {artistName && <span className="QueryCard-SongName-artist">{artistName}</span>}
-      <QueryCardPlayButton />
+      {imageUrl && (
+        <img src={imageUrl} alt="SongIMG" className="QueryCard-SongIMG" />
+      )}
+      <div className="QueryCardSong-options">
+        <div className="QueryCard-info">
+          {songName && <p className="QueryCard-SongName">{songName}</p>}
+          {artistName && (
+            <span className="QueryCard-SongName-artist">{artistName}</span>
+          )}
+        </div>
+        <button className="QueryCardPlayButton">
+          <img src={PlayIMG} alt="Play" className="QueryCardPlayButton-img" onClick={onClick} />
+        </button>
+      </div>
     </div>
   );
 };
